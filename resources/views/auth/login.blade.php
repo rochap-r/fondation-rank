9b51038e4e9ae73fb082954a9ebcde708333665b
Rodrigue <rodriguechot@gmail.com>
2024-06-17 14:48:50 +0200

    mis à jour de la login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
     <meta name="description" content="Fondation RANK">
-    <meta name="author" content="Fondation Ruwej A Nkond, Oeuvrant dans la promotion de l'éducation et le dévéloppement rural">
+    <meta name="author"
+        content="Fondation Ruwej A Nkond, Oeuvrant dans la promotion de l'éducation et le dévéloppement rural">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'Authentification')</title>
@@ -18,7 +19,7 @@
 </head>
 
 <body class="bg-primary">
-    
+
     <!-- Login Form -->
     <div class="row align-items-center justify-content-center vh-100">
         <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-6">
@@ -35,10 +36,11 @@
                 <div class="card-body p-0">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
+                        @csrf
                         <div class="form-group">
-                            <input type="text" class="form-control" name="email" value=""
-                                :value="old('email')" placeholder="Entrez votre email" autofocus
-                                autocomplete="username" required>
+                            <label for="email">Adresse e-mail</label>
+                            <input id="email" type="email" class="form-control" name="email" value=""
+                                required autofocus placeholder="Entrez l'e-mail">
                             <span class="text-danger">
                                 @error('email')
                                     {{ $message }}
@@ -47,8 +49,13 @@
                         </div>
 
                         <div class="form-group">
-                            <input type="password" class="form-control" name="password"
-                                placeholder="Entrez votre Mot de passe" autocomplete="current-password" required>
+                            <label for="password">Mot de passe
+                                <a href="{{ route('password.request') }}" class="float-right">
+                                    Mot de passe oublié?
+                                </a>
+                            </label>
+                            <input id="password" type="password" class="form-control" name="password" required data-eye
+                                placeholder="Entrer le mot de passe" autocomplete="off">
                             <span class="text-danger">
                                 @error('password')
                                     {{ $message }}
@@ -56,14 +63,17 @@
                             </span>
                         </div>
 
+                        <div class="form-group">
+                            <div class="custom-checkbox custom-control">
+                                <input type="checkbox" name="remember" id="remember" class="custom-control-input">
+                                <label for="remember" class="custom-control-label">Se souvenir de moi</label>
+                            </div>
+                        </div>
+
                         <button
                             class="btn btn-primary w-100 text-uppercase text-white rounded-2 lh-34 ff-heading fw-bold shadow">
                             Connexion
                         </button>
-
-                        <p class="d-flex align-items-center justify-content-between mt-4 mb-4">Mot de passe oublié? <a
-                                href="{{ route('password.request') }}"
-                                class="text-primary fw-bold text-decoration-underline">Cliquez ici</a></p>
 
                     </form>
                 </div>
